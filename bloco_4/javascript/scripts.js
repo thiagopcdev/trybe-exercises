35ea6162f3bfeb8464cbc670782b0e8473c90460
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-22 16:05:57 -0300

    Exercicio 3

--- bloco_4/javascript/scripts.js
+++ bloco_4/javascript/scripts.js
@@ -1,3 +1,4 @@
+// Exercicio 1
 const name = "Thiago";
 const birthCity = "Cachoeira Paulista";
 let birthYear = 1992;
@@ -6,10 +7,23 @@ let birthYear = 1992;
 //console.log(birthCity);
 //console.log(birthYear);
 
+// Exercicio 2
 var base = 5;
 var altura = 8;
 var area = base * altura;
-console.log(area);
+//console.log(area);
 
 var perimetro = base + altura + base + altura; // 4 lados de um retangulo
-console.log(perimetro);
+//console.log(perimetro);
+
+// Exercicio 3
+
+var nota = 90;
+
+if (nota >= 80){
+    console.log("Congratulations, você vai aprovado(a)!");
+}else if (nota < 80 && nota >= 60){
+    console.log("Vocẽ está na nossa lista de espera.");
+}else{
+    console.log("Você foi reprovado(a)!");
+}
