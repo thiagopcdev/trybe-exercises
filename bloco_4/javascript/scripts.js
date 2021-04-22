a18aab0c076eade97e470168681271327ead036a
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-22 16:24:04 -0300

    Exercicio 3

--- bloco_4/javascript/scripts.js
+++ bloco_4/javascript/scripts.js
@@ -20,10 +20,27 @@ var perimetro = base + altura + base + altura; // 4 lados de um retangulo
 
 var nota = 90;
 
-if (nota >= 80){
-    console.log("Congratulations, você vai aprovado(a)!");
-}else if (nota < 80 && nota >= 60){
-    console.log("Vocẽ está na nossa lista de espera.");
-}else{
-    console.log("Você foi reprovado(a)!");
+//if (nota >= 80){
+//    console.log("Congratulations, você vai aprovado(a)!");
+//}else if (nota < 80 && nota >= 60){
+//    console.log("Vocẽ está na nossa lista de espera.");
+//}else{
+//    console.log("Você foi reprovado(a)!");
+//}
+
+//Exericicio 4
+var status = "aprovado"
+switch (status){
+    case ("aprovado"):
+        console.log("Aprovado");
+        break;
+    case ("lista"):
+        console.log("Lista");
+        break;
+    case ("reprovado"):
+        console.log("Reprovado");
+        break;
+    default:
+        console.log("Não se aplica");
+        break;
 }
