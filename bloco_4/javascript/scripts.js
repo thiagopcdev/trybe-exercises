e0cf5ceb3393cb5bf21aa30c447d0ad9e3d2e59d
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-22 15:59:02 -0300

    Exercicio 2

--- bloco_4/javascript/scripts.js
+++ bloco_4/javascript/scripts.js
@@ -2,6 +2,14 @@ const name = "Thiago";
 const birthCity = "Cachoeira Paulista";
 let birthYear = 1992;
 
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
+//console.log(name);
+//console.log(birthCity);
+//console.log(birthYear);
+
+var base = 5;
+var altura = 8;
+var area = base * altura;
+console.log(area);
+
+var perimetro = base + altura + base + altura; // 4 lados de um retangulo
+console.log(perimetro);
