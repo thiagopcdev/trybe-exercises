dcf496ec84c54303fb903ddc90ddeeec56750ff2
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-23 18:20:44 -0300

    Agora na pratica, exercicios bonus

--- bloco_4/javascript/day_2/exercicios.js
+++ bloco_4/javascript/day_2/exercicios.js
@@ -39,7 +39,8 @@ for(let name of names) {
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//1:
+//1: Percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+    
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     for (let i = 0; i < numbers.length; i += 1){
         console.log(numbers[i]);
@@ -50,7 +51,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         console.log(num);
     }
 
-//2:
+//2: Some todos os valores contidos no array e imprima o resultado;
 
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let sum = 0;
@@ -58,7 +59,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         sum += numbers[i];
     }
         console.log(sum);
-//3:
+
+//3: Calcule e imprima a média aritmética dos valores contidos no array;
 
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let sum = 0;
@@ -67,7 +69,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     }
     let media = (sum / numbers.length);
       console.log(media);
-//4:
+
+//4: Caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let sum = 0;
@@ -81,7 +84,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         console.log("Média menor ou igual a 20!");
     }
     
-//5:
+//5: Utilizando for , descubra qual o maior valor contido no array e imprima-o;
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let maior = 0;
     for(let i = 0; i < numbers.length; i += 1){
@@ -90,7 +93,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     }
     console.log(maior);
 
-//6:
+//6: Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let qntImpar= 0;
@@ -104,7 +107,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         console.log("Nenhum valor ímpar encontrado!");
     }
     
-//7:
+//7: Utilizando for , descubra qual o menor valor contido no array e imprima-o;
     let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let menor = numbers[0];
     for(let i = 0; i < numbers.length; i += 1){
@@ -112,13 +115,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         if (numbers[i] < menor){ menor = numbers[i];}
     }
     console.log(menor);
-//8: 
+//8: Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
     let array = [];
     for(let i = 1; i <=25; i += 1){
         array.push(i * 2)
     } 
     console.log(array);
-//9: 
+//9: Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
     let array = [];
     for(let i = 1; i <=25; i += 1){
@@ -128,3 +131,56 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
         array[i] = (array[i] / 2);
     } 
     console.log(array);
+
+//Bonus:
+
+//Organiza em ordem crescente:
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    
+    for (let index = 1; index < numbers.length; index += 1) {
+        for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+            if (numbers[index] < numbers[secondIndex]) {
+                let position = numbers[index];
+                numbers[index] = numbers[secondIndex];
+                numbers[secondIndex] = position;
+            }
+        }
+    }
+    for (let number of numbers){
+        console.log(number);
+    }
+
+//Organiza em ordem decrescente:
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    
+        for (let index = 1; index < numbers.length; index += 1) {
+            for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+                if (numbers[index] > numbers[secondIndex]) {
+                    let position = numbers[index];
+                    numbers[index] = numbers[secondIndex];
+                    numbers[secondIndex] = position;
+                }
+            }
+         }
+        for (let number of numbers){
+            console.log(number);
+        }
+
+//Novo Array multiplicado pelo seguinte com final multiplicado por 2:
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let numbers2 = [];
+    
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (i+1 != numbers.length){
+            numbers2.push(numbers[i] * numbers[i+1]);
+        }else{
+            numbers2.push(numbers[i] * 2);
+        }
+       
+    }
+    for (let number of numbers2){
+       console.log(number);
+    }
