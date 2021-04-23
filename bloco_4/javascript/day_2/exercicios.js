6883af25442de9c0c146de798a8b0c149b046228
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-23 16:00:25 -0300

    Agora na pratica 1-3

--- bloco_4/javascript/day_2/exercicios.js
+++ bloco_4/javascript/day_2/exercicios.js
@@ -34,3 +34,36 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for(let name of names) {
   console.log(name);
 }
+
+//Agora na pratica
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1:
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    for (let i = 0; i < numbers.length; i += 1){
+        console.log(numbers[i]);
+    }
+        // ou usando o for of
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    for (let num of numbers){
+        console.log(num);
+    }
+
+//2:
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i += 1){
+        sum += numbers[i];
+    }
+        console.log(sum);
+//3:
+
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i += 1){
+        sum += numbers[i];
+    }
+    let media = sum / numbers.length
+      console.log(media);
