120f1fa42dedbbb502373262f1b9ee12ecd986e6
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-23 15:24:52 -0300

    Exercicio 2 do dia 2

--- bloco_4/javascript/day_2/exercicios.js
+++ bloco_4/javascript/day_2/exercicios.js
@@ -1,6 +1,13 @@
 //Exercicio 1
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf('Serviços');
+let menuServices = menu[1];
 
 console.log(menuServices);
+
+//Exercicio 2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
