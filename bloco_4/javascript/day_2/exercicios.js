39b3f9e2ae35ea1bd4a6d5073c0a82865c9ca902
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-23 15:49:00 -0300

    Exercicios do conteudo

--- bloco_4/javascript/day_2/exercicios.js
+++ bloco_4/javascript/day_2/exercicios.js
@@ -11,3 +11,26 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
 console.log(indexOfPortfolio);
+
+//Exercicio 3
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+console.log(menu);
+
+//For
+//Exercicio 1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let i = 0; i < groceryList.length; i += 1){
+    console.log(groceryList[i]);
+}
+
+//For of
+//Exercicio 1
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let name of names) {
+  console.log(name);
+}
