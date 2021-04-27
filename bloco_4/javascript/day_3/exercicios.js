b571bc7d386511174ffb5f62cab421d206d46106
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-27 15:15:54 -0300

    Iniciando dia 4

--- bloco_4/javascript/day_3/exercicios.js
+++ bloco_4/javascript/day_3/exercicios.js
@@ -68,3 +68,37 @@ for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
   controlRight += 1;
   controlLeft -= 1
 };
+
+//Bonus 1:
+
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
+
+
+// Bonus 2: 
+
+let divisors = 1;
+let numberToCheck = 25;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
