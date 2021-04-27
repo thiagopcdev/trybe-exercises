176b9a0d7d429dbb67d73422c11a1f64d150a956
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-27 15:41:53 -0300

    Exercicio 1 do conteudo

---
+++ bloco_4/javascript/day_4/exercicios.js
@@ -0,0 +1,16 @@
+let athlete = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+    
+}
+
+console.log (`A jogadora ${athlete.name, athlete.lastName} tem ${athlete.age} de idade.`)
+
+athlete['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(`A jogadora ${athlete.name, athlete.lastName} foi eleita a melhor do mundo por ${athlete.bestInTheWorld.length} vezes.`)
+
+console.log(`A jogadora possui ${athlete.medals.golden} medalhas de ouro e ${athlete.medals.silver} medalhas de prata.
+`)
