3505d4528c2a0537b2781ddbee02a427c4f293a2
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-27 15:59:39 -0300

    Exercicio 2 do conteudo

--- bloco_4/javascript/day_4/exercicios.js
+++ bloco_4/javascript/day_4/exercicios.js
@@ -1,3 +1,5 @@
+//Exercicio 1 do conteúdo
+
 let athlete = {
     name: 'Marta',
     lastName: 'Silva',
@@ -14,3 +16,25 @@ console.log(`A jogadora ${athlete.name, athlete.lastName} foi eleita a melhor do
 
 console.log(`A jogadora possui ${athlete.medals.golden} medalhas de ouro e ${athlete.medals.silver} medalhas de prata.
 `)
+
+//Exercicio 2 do conteúdo
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge'
+  }
+
+    for(let i in names){
+        console.log('Olá, '+ names[i]);
+    }
+
+let carro = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+}
+    for (let i in carro){
+        console.log(i + ': '+ carro[i]);
+    }
+
